faa60d2e5eacb0dcba32120af1af1a5a40febb99
Kevin Cooper <cooperka92@gmail.com>
2020-12-22 22:51:15 -0800

    :art: Add back profile photo shadow

--- src/components/App/MainHeader/component.js
+++ src/components/App/MainHeader/component.js
@@ -1,14 +1,16 @@
 // @flow
 
 import React from 'react';
+import Box from '@material-ui/core/Box';
 
 import profilePhoto from '../../../images/profile-2017-web@800.jpg';
 
 const MainHeader = () => {
   return (
     <div className="main-section">
-      {/* TODO: Add back shadow via MUI. */}
-      <img className="profile-photo" src={profilePhoto} alt="profile" />
+      <Box className="profile-photo" boxShadow={2}>
+        <img className="profile-photo" src={profilePhoto} alt="profile" />
+      </Box>
       <div className="main-title">Kevin Cooper</div>
       <div className="main-subtitle">
         Improv comedy. Social justice. Ethical technology. Software consultant.
