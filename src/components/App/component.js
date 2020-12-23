fa164e1971cdba12f6da16ad243b19aac0046a1b
Kevin Cooper <cooperka92@gmail.com>
2020-12-22 17:24:38 -0800

    :rocket: Install MUI

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,27 +1,43 @@
 // @flow
 
 import React, { Component } from 'react';
+import { compose } from 'ramda';
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  CssBaseline,
+  withStyles,
+} from '@material-ui/core';
 
 import MainHeader from './MainHeader/component';
 import SocialLinks from './SocialLinks/component';
 import ProjectLinks from './ProjectLinks/component';
 import Footer from './Footer/component';
 
+import 'fontsource-roboto';
+import theme from './theme';
 import './styles.css';
+
+const muiTheme = createMuiTheme(theme);
 
 type Props = {};
 
 class App extends Component<Props> {
   render() {
     return (
-      <div className="App">
-        <MainHeader />
-        <ProjectLinks />
-        <SocialLinks />
-        <Footer />
-      </div>
+      <MuiThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <div className="App">
+          <MainHeader />
+          <ProjectLinks />
+          <SocialLinks />
+          <Footer />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+const styles = {};
+
+export default compose(withStyles(styles))(App);
