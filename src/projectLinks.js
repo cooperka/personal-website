c60ebf0d25f46d509859bd798b1d7e1165ef0105
Kevin Cooper <cooperka92@gmail.com>
2025-10-29 15:11:49 -0400

    :sparkles: add more detail to project snippets

--- src/projectLinks.js
+++ src/projectLinks.js
@@ -1,13 +1,14 @@
 export default [
   {
     name: 'Two-Hand Piano',
-    description: 'Web piano built with Tone.js',
+    description:
+      'Web piano built with Tone.js for fun (since nothing else existed at the time that supported multi-key-press tones).',
     url: 'https://piano.cooperka.com/',
   },
   {
     name: 'NeuroName',
     description:
-      'Game to improve your memory by matching celebrity names to faces.',
+      'Game to improve your memory by matching celebrity names to faces. Content scraped from Wikipedia.',
     url: 'https://name.cooperka.com/',
   },
 ];
